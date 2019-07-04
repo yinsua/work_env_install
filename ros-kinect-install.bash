6b5775f2adbceab83359071997347f5a45fa4bfb
yinsua <ausniy@gmail.com>
2019-07-04 10:08:20 +0800

    change source from ros office to ustc

--- ros-kinect-install.bash
+++ ros-kinect-install.bash
@@ -1,4 +1,4 @@
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' &&
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update &&
 yes | sudo apt-get install ros-kinetic-desktop-full &&
