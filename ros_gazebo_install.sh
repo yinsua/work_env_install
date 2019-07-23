cf2d463810bb332f6128a8424d2a9edc22883171
yinsua <ausniy@gmail.com>
2019-07-23 15:51:55 +0800

    Create ros_gazebo_install.sh

---
+++ ros_gazebo_install.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+sudo apt-get install ros-kinetic-gazebo-ros ros-kinetic-gazebo-ros-control ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-controller-interface ros-kinetic-controller-manager
